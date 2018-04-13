2645e6a6993b7a8453422f6ac970afbd21d15c3e
Prakhar <prakhar.9212@gmail.com>
2018-04-14 02:53:42 +0530

    commit

--- Regiter/settings.py
+++ Regiter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,8 +26,8 @@ SECRET_KEY = '2+u8f^l4%$pmdl=l5q(zis+#)65!zowlk^3ncq&j8lvc4qfxlw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-  '**.herokuapp.com',
+ALLOWED_HOSTS = ['127.0.0.1',
+  '.herokuapp.com',
 ]
 
 
@@ -90,7 +91,8 @@ DATABASES = {
     }
 }
 
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
@@ -138,7 +140,8 @@ EMAIL_HOST_USER = 'youremail@gmail.com'
 EMAIL_HOST_PASSWORD = 'yourpassword'
 EMAIL_PORT = 587
 STATIC_URL = '/static/'
-STATIC_ROOT="Profile/static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'Profile/static')
+
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'home'
